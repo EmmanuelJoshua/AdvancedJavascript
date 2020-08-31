ac352bc11760b183be65da900f6056ed648e14f1
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2020-08-31 15:37:36 +0100

    Async and Await

--- Script3.js
+++ Script3.js
@@ -86,5 +86,105 @@ const urls2 = [
         console.log('Erroorrrrr!')
     })
 
-// #6) Change one of your urls above to make it incorrect and fail the promise
-// does your catch block handle it?
+//ASYNC and AWAIT
+
+//provides another way to do async tasks without using fetch and a .then block
+
+//fetch way
+
+fetch('https://swapi.co/api/people/4')
+    .then(response => response.json())
+    .then(console.log)
+
+Promise.all(urls2.map(url => {
+        return fetch(url).then(response => response.json())
+    })).then(values => {
+        console.log(values[0])
+        console.log(values[1])
+        console.log(values[2])
+        console.log(values[3])
+    }).catch(() => {
+        console.log('Erroorrrrr!')
+    })
+
+
+//async way
+
+async function fetchUsers(){
+    const resp = await fetch('https://swapi.co/api/people/4');
+    const data = await resp.json();
+    console.log(data)
+}
+async function fetchAllData(){
+    try{
+        const [char1, char2,  char3, char4] = Promise.all(urls2.map(url => {
+            return fetch(url).then(response => response.json())
+        }));
+        console.log(char1)
+        console.log(char2)
+        console.log(char3)
+        console.log(char4)
+    }catch(error){
+        console.log('error', error)
+    }
+
+}
+
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch('https://swapi.co/api/starships/9/')
+  .then(response => response.json())
+  .then(console.log)
+
+  async function getShip(){
+      const response = await fetch('https://swapi.co/api/starships/9/')
+      const data = await response.json()
+      console.log(data)
+  }
+
+// #2) ADVANCED: Update the function below from the video to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
+const urls3 = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls3.map(async function(url) {
+    const response = await fetch(url);
+    return response.json();
+}));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+
+async function myFetch(url){
+    try{
+        return await fetch(url).then(resp => resp.json())
+    }catch(error){
+        console.log('ooooooops', error)
+    }
+  
+}
+
+// #3) Add a try catch block to the #2 solution in order to catch any errors.
+// Now chnage one of the urls so you console.log your error with 'ooooooops'
+
+const getData = async function() {
+    try {
+      const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+          const response = await fetch(url);
+          return response.json();
+      }));
+      console.log('users', users);
+      console.log('posta', posts);
+      console.log('albums', albums);
+    } catch (err) {
+      console.log('ooooooops', err);
+    }
+  }
